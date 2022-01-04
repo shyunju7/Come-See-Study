575fa445a088440ff951119f801713327eba5742
shyunju7 <shyunju7@naver.com>
2022-01-04 21:43:51 +0900

    Comment delete Book

--- book-service-app/src/Route/Intro/IntroPresenter.js
+++ book-service-app/src/Route/Intro/IntroPresenter.js
@@ -1,10 +1,8 @@
 import React from "react";
-import Book from "../../Components/Book";
 import LibraryBook from "../../Components/BookWithLib";
 import * as S from "../Intro/style";
 const IntroPresenter = () => (
   <S.Container>
-    {/* <Book /> */}
     <LibraryBook />
   </S.Container>
 );
