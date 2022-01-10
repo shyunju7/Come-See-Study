76513c2f17d3c0603330b8d2406b653c1070838b
shyunju7 <shyunju7@naver.com>
2022-01-10 20:26:48 +0900

    Feat set Main router

--- book-service-app/src/Router.js
+++ book-service-app/src/Router.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Intro from "../src/Route/Intro";
 import Main from "../src/Route/Main";
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/main/*" element={<Main />} />
+        <Route path="/:id" element={<Main />} />
       </Routes>
     </Router>
   );
