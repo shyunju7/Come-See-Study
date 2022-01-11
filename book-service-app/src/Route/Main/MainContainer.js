77458d3379ee0c002e0b9baac975f6b45408b660
shyunju7 <shyunju7@naver.com>
2022-01-11 18:54:38 +0900

    Feat set custom cursor

--- book-service-app/src/Route/Main/MainContainer.js
+++ book-service-app/src/Route/Main/MainContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MainPresenter from "./MainPresenter";
 
 const testInfo = [
@@ -39,7 +39,23 @@ const testInfo = [
 
 const MainContainer = () => {
   const [show, setShow] = useState(false);
-  return <MainPresenter attrInfo={testInfo} show={show} setShow={setShow} />;
+  const textRef = useRef();
+  useEffect(() => {
+    document.addEventListener("mousemove", (e) => {
+      textRef.current.style.position = "absolute";
+      textRef.current.style.cursor = "default";
+      textRef.current.style.left = e.clientX + "px";
+      textRef.current.style.top = e.clientY + "px";
+    });
+  });
+  return (
+    <MainPresenter
+      attrInfo={testInfo}
+      show={show}
+      setShow={setShow}
+      textRef={textRef}
+    />
+  );
 };
 
 export default MainContainer;
--- book-service-app/src/Route/Main/MainPresenter.js
+++ book-service-app/src/Route/Main/MainPresenter.js
@@ -2,11 +2,12 @@ import React from "react";
 import Carousel from "../../Components/Carousel";
 import Menu from "../../Components/Menu";
 
-const MainPresenter = ({ attrInfo, show, setShow }) =>
+const MainPresenter = ({ attrInfo, show, setShow, textRef }) =>
   show ? (
     <Menu setShow={setShow} />
   ) : (
     <div>
+      <p ref={textRef}>click</p>
       <button onClick={() => setShow((prev) => !prev)}>menu</button>
     </div>
   );
