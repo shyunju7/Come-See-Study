2c08b91f7ebd0650ee41c14a8d771ed450310f4c
shyunju7 <shyunju7@naver.com>
2022-01-11 19:16:18 +0900

    Feat set Lecture router

--- book-service-app/src/Router.js
+++ book-service-app/src/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Main from "../src/Route/Main";
+import Lecture from "../src/Route/Lecture";
 const AppRouter = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/:attrId/lecture/:lectureNum" element={<Lecture />} />
       </Routes>
     </Router>
   );
