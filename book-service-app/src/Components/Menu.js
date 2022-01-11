989fc72a6c20d3cd4643c5bf66fca404acec19fa
shyunju7 <shyunju7@naver.com>
2022-01-11 18:05:10 +0900

    Feat set menu animation

--- book-service-app/src/Components/Menu.js
+++ book-service-app/src/Components/Menu.js
@@ -1,16 +1,40 @@
-import react from "react";
+import react, { useRef } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: red;
+  animation: fadein 2s;
+
+  @keyframes fadein {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+
+  @keyframes fadeout {
+    from {
+      opacity: 1;
+    }
+    to {
+      opacity: 0;
+    }
+  }
 `;
 
 const Menu = ({ setShow }) => {
+  const menuRef = useRef();
+  const closeMenu = () => {
+    menuRef.current.style = "animation: fadeout 2s;";
+    setShow(false);
+  };
   return (
-    <Container>
-      <button onClick={() => setShow(false)}>x</button>
+    <Container ref={menuRef}>
+      <button onClick={closeMenu}>x</button>
     </Container>
   );
 };
