acd74c9edbcbd08f8279ad61849cf6cb4faded56
shyunju7 <shyunju7@naver.com>
2022-01-13 02:47:14 +0900

    Add Previe page

---
+++ book-service-app/src/Route/Preview/PreviewContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import PreviewPresenter from "./PreviewPresenter";
+
+const PreviewContainer = () => {
+  return <PreviewPresenter />;
+};
+
+export default PreviewContainer;
---
+++ book-service-app/src/Route/Preview/PreviewPresenter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const PreviewPresenter = () => <div></div>;
+
+export default PreviewPresenter;
