ac840aea47db9ac5cceb6c6f240dc5aa2b8152ed
shyunju7 <shyunju7@naver.com>
2022-01-12 16:34:27 +0900

    Style Button stlye & set cursor hover

--- book-service-app/src/Route/Main/style.js
+++ book-service-app/src/Route/Main/style.js
@@ -1,7 +1,57 @@
 import styled from "styled-components";
-
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  background-color: transparent;
+  position: relative;
+  overflow: hidden;
+`;
+
+export const CursorDescription = styled.div`
+  position: absolute;
+  z-index: 1000;
+  cursor: default;
+  font-size: 18px;
+  ::before {
+    content: "click";
+  }
+`;
+
+export const Menu = styled.div`
+  position: absolute;
+  top: 50px;
+  left: 50px;
+  z-index: 1000;
+  &:hover + ${CursorDescription} {
+    ::before {
+      content: "";
+    }
+    ::after {
+      content: "menu";
+    }
+    background-color: #102542;
+    color: #fff;
+    border-radius: 50%;
+    padding: 21px 10px;
+  }
+`;
+
+export const PrevButton = styled.button`
+  border: none;
+  outline: none;
+  position: absolute;
+  top: 50%;
+  left: 50px;
+  z-index: 1000;
+  background-color: transparent;
+`;
+
+export const NextButton = styled.button`
+  border: none;
+  outline: none;
+  position: absolute;
+  top: 50%;
+  right: 50px;
+  z-index: 1000;
   background-color: transparent;
 `;
