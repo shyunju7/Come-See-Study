ddbd981e718619471acc696d18982aece8ce3c7a
shyunju7 <shyunju7@naver.com>
2022-01-21 02:20:01 +0900

    Feat set page(bg,border,transition) & notfound

--- book-service-app/src/Route/Preview/PreviewPresenter.js
+++ book-service-app/src/Route/Preview/PreviewPresenter.js
@@ -1,25 +1,35 @@
 import React from "react";
-import ColorPreview from "../../Components/ColorPreview";
-import TextPreview from "../../Components/TextPreview";
+import ColorPreview from "../../Components/PreviewColor";
+import TextPreview from "../../Components/PreviewText";
+import PreviewBackground from "../../Components/PreviewBackground";
+import PreviewBorder from "../../Components/PreviewBorder";
+import PreviewTransition from "../../Components/PreviewTransition";
 import * as S from "./style";
 import PrevIcon from "../../assets/prev-icon.png";
 import NextIcon from "../../assets/next-icon.png";
+import NotFound from "../../Pages/NotFound";
 
 const pagePreview = {
   color: <ColorPreview />,
   text: <TextPreview />,
+  background: <PreviewBackground />,
+  border: <PreviewBorder />,
+  transition: <PreviewTransition />,
 };
 
-const PreviewPresenter = ({ attrId }) => (
-  <div>
-    {pagePreview[attrId]}
-    <S.PrevButton to={`/${attrId}`}>
-      <img src={PrevIcon} alt="Prev" width="32px" />
-    </S.PrevButton>
-    <S.NextButton to={`/${attrId}/lecture/1`}>
-      <img src={NextIcon} alt="Next" width="32px" />
-    </S.NextButton>
-  </div>
-);
+const PreviewPresenter = ({ attrId }) =>
+  !pagePreview[attrId] ? (
+    <NotFound />
+  ) : (
+    <div>
+      {pagePreview[attrId]}
+      <S.PrevButton to={`/${attrId}`}>
+        <img src={PrevIcon} alt="Prev" width="32px" />
+      </S.PrevButton>
+      <S.NextButton to={`/${attrId}/lecture/1`}>
+        <img src={NextIcon} alt="Next" width="32px" />
+      </S.NextButton>
+    </div>
+  );
 
 export default PreviewPresenter;
