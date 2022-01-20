6d30a936179817cb1440a8ba203cf61477f79f16
shyunju7 <shyunju7@naver.com>
2022-01-20 22:04:02 +0900

    Style set NotFound

--- book-service-app/src/Pages/NotFound.js
+++ book-service-app/src/Pages/NotFound.js
@@ -1,5 +1,24 @@
 import react from "react";
+import styled from "styled-components";
 
-const NotFound = () => <div>NotFound!!</div>;
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  color: red;
+`;
+const NotFound = () => (
+  <Container>
+    <Text>Not Found Page...!</Text>
+  </Container>
+);
 
 export default NotFound;
