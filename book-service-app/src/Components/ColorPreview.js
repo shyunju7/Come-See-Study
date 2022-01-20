4aebec3342fc5b9cdd8999e0b5515ddf3c85bfc2
shyunju7 <shyunju7@naver.com>
2022-01-20 22:58:58 +0900

    Style RGB slider

--- book-service-app/src/Components/ColorPreview.js
+++ book-service-app/src/Components/ColorPreview.js
@@ -1,4 +1,4 @@
-import react, { useEffect, useState } from "react";
+import react, { useState } from "react";
 import styled from "styled-components";
 import { ReactComponent as Rect } from "../assets/rect-img.svg";
 import { ReactComponent as Triangle } from "../assets/triangle-img.svg";
@@ -15,31 +15,27 @@ const Container = styled.div`
 
 const Chapter = styled.p`
   font-size: 1rem;
-  font-weight: bold;
-  margin-bottom: 12px;
+  margin-top: 45px;
+  font-weight: 600;
 `;
 
 const GuideText = styled.h2`
-  font-size: 1.25rem;
-  font-weight: bold;
+  font-size: 1.12rem;
+  font-weight: 600;
+  margin-top: 100px;
+  margin-bottom: 100px;
 `;
 
 const RoundCharacter = styled(Round)`
-  margin-right: 30px;
+  margin-bottom: 42px;
 `;
 
 const RectCharacter = styled(Rect)`
-  margin-right: 30px;
+  margin-bottom: 42px;
 `;
 
 const TriangleChracter = styled(Triangle)`
-  margin-top: 10px;
-`;
-
-const ImageWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin-top: 100px;
+  margin-bottom: 12px;
 `;
 
 const RangeSliderWrapper = styled.div`
@@ -49,7 +45,13 @@ const RangeSliderWrapper = styled.div`
 const RGBASliderList = styled.div`
   display: flex;
   flex-direction: column;
-  margin-right: 32px;
+  margin-right: 46px;
+  align-items: center;
+`;
+
+const RGBText = styled.label`
+  font-size: 1.1rem;
+  margin-right: 43px;
 `;
 
 const RGBASlider = styled.input`
@@ -151,75 +153,60 @@ const ColorPreview = () => {
 
   return (
     <Container>
-      <Chapter>CSS #2</Chapter>
+      <Chapter>CSS #1</Chapter>
       <GuideText>Change your color</GuideText>
-      <ImageWrapper>
-        <RoundCharacter
-          fill={`rgba(${circleRgba.r},${circleRgba.g}, ${circleRgba.b}, ${circleRgba.a})`}
-        />
-        <RectCharacter
-          fill={`rgba(${rectRgba.r},${rectRgba.g}, ${rectRgba.b}, ${rectRgba.a})`}
-        />
-        <TriangleChracter
-          fill={`rgba(${triangleRgba.r},${triangleRgba.g}, ${triangleRgba.b}, ${triangleRgba.a})`}
-        />
-      </ImageWrapper>
+
       <RangeSliderWrapper>
         <RGBASliderList>
-          <span>
-            <label>R</label>
-            <RGBASlider
-              type="range"
-              min="0"
-              max="256"
-              step="1"
-              defaultValue={circleRgba.r}
-              onChange={(e) =>
-                setCircleRgba({ ...circleRgba, r: Number(e.target.value) })
-              }
-            />
-          </span>
-          <span>
-            <label>G</label>
-            <RGBASlider
-              type="range"
-              min="0"
-              max="256"
-              step="1"
-              defaultValue={circleRgba.g}
-              onChange={(e) =>
-                setCircleRgba({ ...circleRgba, g: Number(e.target.value) })
-              }
-            />
-          </span>
-          <span>
-            <label>B</label>
-            <RGBASlider
-              type="range"
-              min="0"
-              max="256"
-              step="1"
-              defaultValue={circleRgba.b}
-              onChange={(e) =>
-                setCircleRgba({ ...circleRgba, b: Number(e.target.value) })
-              }
-            />
-          </span>
-          <span>
-            <label>A</label>
-            <RGBASlider
-              type="range"
-              min="0"
-              max="1"
-              step="0.1"
-              defaultValue={circleRgba.a}
-              onChange={(e) =>
-                setCircleRgba({ ...circleRgba, a: parseFloat(e.target.value) })
-              }
-            />
-          </span>
+          <RoundCharacter
+            fill={`rgba(${circleRgba.r},${circleRgba.g}, ${circleRgba.b}, ${circleRgba.a})`}
+          />
+          <RGBASlider
+            type="range"
+            min="0"
+            max="256"
+            step="1"
+            defaultValue={circleRgba.r}
+            onChange={(e) =>
+              setCircleRgba({ ...circleRgba, r: Number(e.target.value) })
+            }
+          />
+
+          <RGBASlider
+            type="range"
+            min="0"
+            max="256"
+            step="1"
+            defaultValue={circleRgba.g}
+            onChange={(e) =>
+              setCircleRgba({ ...circleRgba, g: Number(e.target.value) })
+            }
+          />
+          <RGBASlider
+            type="range"
+            min="0"
+            max="256"
+            step="1"
+            defaultValue={circleRgba.b}
+            onChange={(e) =>
+              setCircleRgba({ ...circleRgba, b: Number(e.target.value) })
+            }
+          />
+          <RGBASlider
+            type="range"
+            min="0"
+            max="1"
+            step="0.1"
+            defaultValue={circleRgba.a}
+            onChange={(e) =>
+              setCircleRgba({ ...circleRgba, a: parseFloat(e.target.value) })
+            }
+          />
         </RGBASliderList>
         <RGBASliderList>
+          <RectCharacter
+            fill={`rgba(${rectRgba.r},${rectRgba.g}, ${rectRgba.b}, ${rectRgba.a})`}
+          />
           <RGBASlider
             type="range"
             min="0"
@@ -262,6 +249,9 @@ const ColorPreview = () => {
           />
         </RGBASliderList>
         <RGBASliderList>
+          <TriangleChracter
+            fill={`rgba(${triangleRgba.r},${triangleRgba.g}, ${triangleRgba.b}, ${triangleRgba.a})`}
+          />
           <RGBASlider
             type="range"
             min="0"
