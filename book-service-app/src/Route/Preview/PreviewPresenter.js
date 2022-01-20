d0b7b0b6f5d4f93ae087deb00c21750a965ae297
shyunju7 <shyunju7@naver.com>
2022-01-20 23:03:30 +0900

    Feat set preview page btn & Style

--- book-service-app/src/Route/Preview/PreviewPresenter.js
+++ book-service-app/src/Route/Preview/PreviewPresenter.js
@@ -1,8 +1,25 @@
 import React from "react";
 import ColorPreview from "../../Components/ColorPreview";
-import NotFound from "../../Pages/NotFound";
-const PreviewPresenter = ({ attrId }) => {
-  return <div>{attrId === "color" ? <ColorPreview /> : <NotFound />}</div>;
+import TextPreview from "../../Components/TextPreview";
+import * as S from "./style";
+import PrevIcon from "../../assets/prev-icon.png";
+import NextIcon from "../../assets/next-icon.png";
+
+const pagePreview = {
+  color: <ColorPreview />,
+  text: <TextPreview />,
 };
+
+const PreviewPresenter = ({ attrId }) => (
+  <div>
+    {pagePreview[attrId]}
+    <S.PrevButton to={`/${attrId}`}>
+      <img src={PrevIcon} alt="Prev" width="32px" />
+    </S.PrevButton>
+    <S.NextButton to={`/${attrId}/lecture/1`}>
+      <img src={NextIcon} alt="Next" width="32px" />
+    </S.NextButton>
+  </div>
+);
 
 export default PreviewPresenter;
--- book-service-app/src/Route/Preview/style.js
+++ book-service-app/src/Route/Preview/style.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Container = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: transparent;
+`;
+
+export const PrevButton = styled(Link)`
+  border: none;
+  outline: none;
+  position: absolute;
+  top: 50%;
+  left: 50px;
+  background-color: transparent;
+  z-index: 1000;
+`;
+
+export const NextButton = styled(Link)`
+  border: none;
+  outline: none;
+  position: absolute;
+  top: 50%;
+  right: 50px;
+  background-color: transparent;
+  z-index: 1000;
 `;
