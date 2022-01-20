32e81720344eb302455acae4e29bed4e7f813367
shyunju7 <shyunju7@naver.com>
2022-01-20 19:08:49 +0900

    Feat set preview router

--- book-service-app/src/Route/Preview/PreviewContainer.js
+++ book-service-app/src/Route/Preview/PreviewContainer.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import PreviewPresenter from "./PreviewPresenter";
 
 const PreviewContainer = () => {
-  return <PreviewPresenter />;
+  const { attrId } = useParams();
+  return <PreviewPresenter attrId={attrId} />;
 };
 
 export default PreviewContainer;
--- book-service-app/src/Route/Preview/PreviewPresenter.js
+++ book-service-app/src/Route/Preview/PreviewPresenter.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ColorPreview from "../../Components/ColorPreview";
-import { useLocation } from "react-router-dom";
 import NotFound from "../../Pages/NotFound";
-const PreviewPresenter = () => {
-  const location = useLocation();
-  return (
-    <div>
-      {location.pathname === "/1-color/preview" ? (
-        <ColorPreview />
-      ) : (
-        <NotFound />
-      )}
-    </div>
-  );
+const PreviewPresenter = ({ attrId }) => {
+  return <div>{attrId === "color" ? <ColorPreview /> : <NotFound />}</div>;
 };
 
 export default PreviewPresenter;
