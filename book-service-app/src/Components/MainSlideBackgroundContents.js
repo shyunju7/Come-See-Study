06d3e77fe6218b2727e5381779369712dd2019a4
shyunju7 <shyunju7@naver.com>
2022-01-20 22:36:38 +0900

    Style remove components

--- book-service-app/src/Components/MainSlideBackgroundContents.js
+++ book-service-app/src/Components/MainSlideBackgroundContents.js
@@ -14,26 +14,8 @@ const ImageBackground = styled.div`
   justify-self: center;
   place-self: center;
 `;
-
-const Title = styled.h2`
-  font-size: 24px;
-  font-weight: bold;
-  margin-bottom: 114px;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-const AttrTitle = styled.h1`
-  font-size: 36px;
-  font-weight: bold;
-  margin-top: 94px;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-`;
-const MainSlideBackgroundContents = ({ title, attrTitle }) => {
-  return (
-    <ImageBackground>
-      <Title>{title}</Title>
-      <AttrTitle>{attrTitle}</AttrTitle>
-    </ImageBackground>
-  );
+const MainSlideBackgroundContents = () => {
+  return <ImageBackground />;
 };
 
 export default MainSlideBackgroundContents;
