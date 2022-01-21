86742aa8669535ce5716118618b825c0ac9b90fc
shyunju7 <shyunju7@naver.com>
2022-01-21 19:17:02 +0900

    Feat set API settings

--- book-service-app/src/api.js
+++ book-service-app/src/api.js
@@ -1,1 +1,9 @@
 import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://3.34.236.99:8081/",
+});
+
+export const attrApi = {
+  getAttrName: (attr) => api.get(`css/main/?attr=${attr}`, {}),
+};
