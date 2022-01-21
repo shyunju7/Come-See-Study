5e1ce629bc9838540b759769ee6860ff3c9e07af
shyunju7 <shyunju7@naver.com>
2022-01-21 21:42:20 +0900

    Style preview

--- book-service-app/src/Components/PreviewBorder.js
+++ book-service-app/src/Components/PreviewBorder.js
@@ -1,7 +1,27 @@
 import react from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fef6c9;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+`;
 
 const PreviewBorder = () => {
-  return <div>PreviewBorder</div>;
+  return (
+    <Container>
+      <Title>PreviewBorder</Title>
+    </Container>
+  );
 };
 
 export default PreviewBorder;
--- book-service-app/src/Components/PreviewTransition.js
+++ book-service-app/src/Components/PreviewTransition.js
@@ -1,7 +1,26 @@
 import react from "react";
+import styled from "styled-components";
 
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fef6c9;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: 700;
+`;
 const PreviewTransition = () => {
-  return <div>PreviewTransition</div>;
+  return (
+    <Container>
+      <Title>PreviewTransition</Title>
+    </Container>
+  );
 };
 
 export default PreviewTransition;
