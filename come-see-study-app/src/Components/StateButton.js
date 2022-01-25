f632e95ed7f663a0e56a7db1bbb9829b5c94843e
shyunju7 <shyunju7@naver.com>
2022-01-25 16:57:02 +0900

    Feat set change textColor

--- come-see-study-app/src/Components/StateButton.js
+++ come-see-study-app/src/Components/StateButton.js
@@ -1,24 +1,47 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
-  width: 120px;
-  background-color: rgba(0, 0, 0, 0.4);
-  border-radius: 16px;
+  width: 64px;
+  height: 30px;
+  background-color: #504428;
+  border-radius: 8px;
   text-align: center;
-  font-size: 20px;
+  vertical-align: middle;
+  font-size: 14px;
   font-weight: 500;
-  line-height: 20px;
-  padding: 10px 4px;
-  color: #fff;
-
+  line-height: 30px;
+  color: ${(props) => (props.color ? props.color : "#fef6c9")};
+  cursor: pointer;
   &:hover {
-    background-color: rgba(0, 0, 0, 0.7);
+    background-color: rgba(80, 68, 40, 0.8);
   }
 `;
 
 const StateButton = ({ stateText, handleCheckedValue }) => {
-  return <Container onClick={handleCheckedValue}>{stateText}</Container>;
+  const [textColor, setTextColor] = useState("#fef6c9");
+  useEffect(() => {
+    switch (stateText) {
+      case "click":
+        setTextColor("#fef6c9");
+        break;
+      case "correct":
+        setTextColor("#ADFF2F");
+        break;
+      case "wrong":
+        setTextColor("#F87060");
+        break;
+      default:
+        setTextColor("#fef6c9");
+        break;
+    }
+  }, [stateText]);
+
+  return (
+    <Container color={textColor} onClick={handleCheckedValue}>
+      {stateText}
+    </Container>
+  );
 };
 
 export default StateButton;
