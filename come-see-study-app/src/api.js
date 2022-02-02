76ce2c2330a58e48b306de55f0a9f592d1bfb777
shyunju7 <shyunju7@naver.com>
2022-02-03 00:44:15 +0900

    Feat make learning api

--- come-see-study-app/src/api.js
+++ come-see-study-app/src/api.js
@@ -7,3 +7,8 @@ const api = axios.create({
 export const attrApi = {
   getAttrName: (attr) => api.get(`css/main/?attr=${attr}`, {}),
 };
+
+export const learningApi = {
+  getLearningPageSettings: (attr, pageNo) =>
+    api.get(`css/learning/?attr=${attr}&pageNo=${pageNo}`, {}),
+};
