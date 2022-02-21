0707d1fa8df1f3f320d3f9aa0881a2fffc4a175e
shyunju7 <shyunju7@naver.com>
2022-02-21 23:56:05 +0900

    Feat set PopupCookie

--- come-see-study-app/src/Route/Syntax/SyntaxPresenter.js
+++ come-see-study-app/src/Route/Syntax/SyntaxPresenter.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import SyntaxImg from "../../assets/syntax-img.png";
 import { IoIosClose } from "react-icons/io";
@@ -7,29 +7,39 @@ const SyntaxPresenter = ({
   setCursorValue,
   cursorValue,
   handleLocateLearningPage,
-}) => (
-  <S.Container>
-    <S.Title>Before You Start ! </S.Title>
-    <S.SubTitle>
-      You must follow correct CSS Syntax to activate your code.
-    </S.SubTitle>
-    <S.Img src={SyntaxImg} />
-    <S.Text>
-      On the learning page, the description of the attribute, the correct answer
-      image, the code window, and the result screen will appear. In the code
-      window, you can write the answer with the above structure and move on to
-      the next page only if it is correct.
-    </S.Text>
+}) => {
+  const [popupCookie, setPopupCookie] = useState(false);
 
-    <S.Button
-      onClick={handleLocateLearningPage}
-      onMouseEnter={() => setCursorValue("close")}
-      onMouseLeave={() => setCursorValue("")}
-    >
-      <IoIosClose size="60px" color="#333333" />
-    </S.Button>
-    <S.CursorDescription ref={textRef}>{cursorValue}</S.CursorDescription>
-  </S.Container>
-);
+  return (
+    <S.Container>
+      <S.Title>Before You Start ! </S.Title>
+      <S.SubTitle>
+        You must follow correct CSS Syntax to activate your code.
+      </S.SubTitle>
+      <S.Img src={SyntaxImg} />
+      <S.Text>
+        On the learning page, the description of the attribute, the correct
+        answer image, the code window, and the result screen will appear. In the
+        code window, you can write the answer with the above structure and move
+        on to the next page only if it is correct.
+      </S.Text>
 
+      <S.Button
+        onClick={handleLocateLearningPage}
+        onMouseEnter={() => setCursorValue("close")}
+        onMouseLeave={() => setCursorValue("")}
+      >
+        <IoIosClose size="60px" color="#333333" />
+      </S.Button>
+      <S.CursorDescription ref={textRef}>{cursorValue}</S.CursorDescription>
+
+      <input
+        type="checkbox"
+        value={popupCookie}
+        onChange={() => setPopupCookie((prev) => !prev)}
+      />
+      <label>Do not open for a day</label>
+    </S.Container>
+  );
+};
 export default SyntaxPresenter;
